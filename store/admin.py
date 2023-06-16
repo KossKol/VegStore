d5f3331c9fd3fb3225cbf40cd701c9b194801e71
Kossster <konstan.kolebovskij2017@yandex.ru>
2023-06-16 19:51:53 +0300

    Revert "Практика_2 commit"
    This reverts commit a285f43fcf3019e55aebf62f9462662877fe39a8.

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,3 @@
-from .models import Product, Category, Discount
 from django.contrib import admin
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Discount)
+# Register your models here.
