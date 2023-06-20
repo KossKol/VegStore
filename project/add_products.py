15f1eb48b6b01514a998da4b924f0d70759455c7
Kossster <konstan.kolebovskij2017@yandex.ru>
2023-06-20 17:34:34 +0300

    Добавление данных через питоновский скрипт конец

--- project/add_products.py
+++ project/add_products.py
@@ -1,23 +1,53 @@
 import django
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 django.setup()
 
 from store.models import Product, Category
 
-# первый способ
+# первый способ(Создание объекта, затем его сохранение)
 category = Category.objects.get(name='Vegetables')
 obj = Product(name='Carrots', description='Морковь', price=120,
-             image='static/products/product-7.jpg',
-             category=category)
+              image='static/products/product-7.jpg',
+              category=category)
 
 obj.save()
 
-# второй способ
+# второй способ(Создание с сохранением)
 obj = Product.objects.create(name='Fruit Juice',
-                            description='Фруктовый сок',
-                            price=120,
-                            image='static/products/product-8.jpg',
-                            category=Category.objects.get(name='Juice'))
+                             description='Фруктовый сок',
+                             price=120,
+                             image='static/products/product-8.jpg',
+                             category=Category.objects.get(name='Juice'))
 
+# третий способ(Групповое сохранение(сохранение блока информации)
+data = ({'name': 'Onion',
+         'price': 120.00,
+         'description': "Лук",
+         'image': 'static/products/product-9.jpg',
+         'category': 'Vegetables'},
+        {'name': 'Apple',
+         'price': 120.00,
+         'description': "Яблоко",
+         'image': 'static/products/product-10.jpg',
+         'category': 'Fruits'},
+        {'name': 'Garlic',
+         'price': 120.00,
+         'description': "Чеснок",
+         'image': 'static/products/product-11.jpg',
+         'category': 'Vegetables'},
+        )
+
+categ = {'Fruits': Category.objects.get(name='Fruits'),
+         'Vegetables': Category.objects.get(name='Vegetables'),
+         }
+
+objects_to_create = [Product(name=val['name'],
+                             description=val['description'],
+                             price=val['price'],
+                             image=val['image'],
+                             category=categ[val['category']]) for val in data]
+
+Product.objects.bulk_create(objects_to_create)
