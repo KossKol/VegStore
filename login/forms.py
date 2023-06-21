f474a884953dab6b3e0fbeebbfdd5f97141497e7
Kossster <konstan.kolebovskij2017@yandex.ru>
2023-06-21 13:45:55 +0300

    Проверки в форме

--- login/forms.py
+++ login/forms.py
@@ -5,13 +5,16 @@ from django.contrib.auth.forms import UsernameField
 from django.forms import EmailField
 
 
+def validate_email(value):
+    if User.objects.filter(email=value).exists():
+        raise ValidationError('Email Already Exists')
+    return value
 
 
 class CustomUserCreationForm(UserCreationForm):
-   email = EmailField()
+    email = EmailField(validators=[validate_email])
 
-
-   class Meta:
-       model = User
-       fields = ['username', 'email', 'password1', 'password2']
-       field_classes = {"username": UsernameField}
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        field_classes = {"username": UsernameField}
