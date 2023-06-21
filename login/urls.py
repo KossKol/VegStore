bf7c30fe566204b9229c05594d353e3eb525430c
Kossster <konstan.kolebovskij2017@yandex.ru>
2023-06-21 12:28:21 +0300

    Работа с формой регистрации

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import LoginView, LogoutView
+from .views import LoginView, LogoutView, CreateAccountView
 
 app_name = 'login'
 
@@ -8,6 +8,7 @@ app_name = 'login'
 urlpatterns = [
    path('', LoginView.as_view(), name="login"),
    path('logout/', LogoutView.as_view(), name="logout"),
+   path('create/', CreateAccountView.as_view(), name="create"),
 ]
 
 
--- login/views.py
+++ login/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
 
 from django.views import View
 from django.http import JsonResponse
@@ -27,3 +29,21 @@ class LogoutView(View):
         if request.user.is_authenticated:
             logout(request)
         return redirect('store:shop')
+
+
+class CreateAccountView(View):
+
+    def get(self, request):
+        return render(request, "login/create_account.html")
+
+    def post(self, request):
+        form = CustomUserCreationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            login(request, user)
+            return redirect('store:shop')
+        return redirect('login:create')
